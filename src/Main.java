8254e6874eecf5d78133dca7990a6065f1523147
Zshxy1013 <838511659@qq.com>
2022-12-07 11:39:42 +0800

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world");
-        System.out.println("12332131231321");
-        System.out.println("aaaaaaaaaa");
+
     }
 }
