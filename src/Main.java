136486ea447680bd5c7ab363c5c4e3a4982dba27
Zshxy1013 <838511659@qq.com>
2022-12-07 11:42:28 +0800

    Revert "initial"
    This reverts commit 8254e687

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
+        System.out.println("Hello world");
+        System.out.println("12332131231321");
+        System.out.println("aaaaaaaaaa");
     }
 }
